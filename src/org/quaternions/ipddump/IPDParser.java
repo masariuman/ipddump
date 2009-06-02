dae638fb05f955852d61ae318fc86591fcd6b238
borkhobs <borkhobs@f63e4cbc-6a43-0410-8324-03926db9f09f>
2009-06-02 01:20:55 +0000

    Reformatted the entire project and added project-specific code formatting styles.

--- src/org/quaternions/ipddump/IPDParser.java
+++ src/org/quaternions/ipddump/IPDParser.java
@@ -10,300 +10,276 @@ import org.quaternions.ipddump.data.Record;
 /**
  * Parse an IPD file and populate a {@link InteractivePagerBackup} with the
  * records.
- *
+ * 
  * @author borkholder
  * @date Jan 13, 2008
  */
-public class IPDParser
-{
-   /**
-    * The name/path of the file to parse.
-    */
-   protected final String fileName;
+public class IPDParser {
+  /**
+   * The name/path of the file to parse.
+   */
+  protected final String fileName;
 
-   /**
-    * Specifies the state of the parser, that is, the current part of the IPD
-    * that the parser is reading.
-    *
-    * @author borkholder
-    * @date Jan 13, 2008
-    */
-   protected enum ReadingState
-   {
-      /**
-       * The file identification header - "Inter@ctive Pager Backup/Restore
-       * File".
-       */
-      HEADER,
+  /**
+   * Specifies the state of the parser, that is, the current part of the IPD
+   * that the parser is reading.
+   * 
+   * @author borkholder
+   * @date Jan 13, 2008
+   */
+  protected enum ReadingState {
+    /**
+     * The file identification header - "Inter@ctive Pager Backup/Restore File".
+     */
+    HEADER,
 
-      /**
-       * The character used to for line feeds in text fields - 0x0A.
-       */
-      LINEFEED,
+    /**
+     * The character used to for line feeds in text fields - 0x0A.
+     */
+    LINEFEED,
 
-      /**
-       * The IPD file version - 0x02.
-       */
-      VERSION,
+    /**
+     * The IPD file version - 0x02.
+     */
+    VERSION,
 
-      /**
-       * The number of databases in this file - 2 bytes in big endian.
-       */
-      DATABASECOUNT,
+    /**
+     * The number of databases in this file - 2 bytes in big endian.
+     */
+    DATABASECOUNT,
 
-      /**
-       * The byte that separates database names - 0x00.
-       */
-      DATABASENAMESEPARATOR,
+    /**
+     * The byte that separates database names - 0x00.
+     */
+    DATABASENAMESEPARATOR,
 
-      /**
-       * The length of the next specified database name - 2 bytes in little
-       * endian. The length includes the terminating null of the name string.
-       */
-      DATABASENAMELENGTH,
+    /**
+     * The length of the next specified database name - 2 bytes in little
+     * endian. The length includes the terminating null of the name string.
+     */
+    DATABASENAMELENGTH,
 
-      /**
-       * The name of the database - size as specified by the preceeding database
-       * name length.
-       */
-      DATABASENAME,
+    /**
+     * The name of the database - size as specified by the preceeding database
+     * name length.
+     */
+    DATABASENAME,
 
-      /**
-       * The 0-based index of the database that contains this record - 2 bytes
-       * little endian.
-       */
-      DATABASEID,
+    /**
+     * The 0-based index of the database that contains this record - 2 bytes
+     * little endian.
+     */
+    DATABASEID,
 
-      /**
-       * The length of the record in number of bytes that follow this value - 4
-       * bytes little endian.
-       */
-      RECORDLENGTH,
+    /**
+     * The length of the record in number of bytes that follow this value - 4
+     * bytes little endian.
+     */
+    RECORDLENGTH,
 
-      /**
-       * The version of the database to which this record belongs - 1 byte.
-       */
-      RECORDDBEVERSION,
+    /**
+     * The version of the database to which this record belongs - 1 byte.
+     */
+    RECORDDBEVERSION,
 
-      /**
-       * A handle of the record within the database (this is a increasing
-       * sequence of integers with the IPD) - 2 bytes little endian.
-       */
-      DATABASERECORDHANDLE,
+    /**
+     * A handle of the record within the database (this is a increasing sequence
+     * of integers with the IPD) - 2 bytes little endian.
+     */
+    DATABASERECORDHANDLE,
 
-      /**
-       * The unique ID of the record within the database - 4 bytes.
-       */
-      RECORDUNIQUEID,
+    /**
+     * The unique ID of the record within the database - 4 bytes.
+     */
+    RECORDUNIQUEID,
 
-      /**
-       * The length of the field data in number of bytes - 2 bytes little
-       * endian.
-       */
-      FIELDLENGTH,
+    /**
+     * The length of the field data in number of bytes - 2 bytes little endian.
+     */
+    FIELDLENGTH,
 
-      /**
-       * The type of the field within the record - 1 byte.
-       */
-      FIELDTYPE,
+    /**
+     * The type of the field within the record - 1 byte.
+     */
+    FIELDTYPE,
 
-      /**
-       * The field data - as long as is specified by the preceeding length.
-       */
-      FIELDDATA;
-   }
+    /**
+     * The field data - as long as is specified by the preceeding length.
+     */
+    FIELDDATA;
+  }
 
-   /**
-    * Creates a new IPDParser that will parse the file at the given path.
-    *
-    * @param fileName
-    *           The path of the file to parse
-    */
-   public IPDParser( String fileName )
-   {
-      this.fileName = fileName;
-   }
+  /**
+   * Creates a new IPDParser that will parse the file at the given path.
+   * 
+   * @param fileName The path of the file to parse
+   */
+  public IPDParser( String fileName ) {
+    this.fileName = fileName;
+  }
 
-   /**
-    * Parses the provided IPD file into an {@link InteractivePagerBackup}.
-    *
-    * @return A new InteractivePagerBackup representing the IPD file
-    * @throws IOException
-    *            Any error in reading the IPD file
-    */
-   public InteractivePagerBackup parse() throws IOException
-   {
-      // Temporary variables used in parsing
-      char lineFeed = 0;
-      int numberOfDatabases = 0;
-      int dbNameLength = 0;
-      int recordRead = 0;
-      int recordDBVersion = 0;
-      int databaseHandle = 0;
-      int fieldLength = 0;
-      int fieldType = 0;
-      int dbID = 0;
-      int recordLength = 0;
-      int uid = 0;
+  /**
+   * Parses the provided IPD file into an {@link InteractivePagerBackup}.
+   * 
+   * @return A new InteractivePagerBackup representing the IPD file
+   * @throws IOException Any error in reading the IPD file
+   */
+  public InteractivePagerBackup parse() throws IOException {
+    // Temporary variables used in parsing
+    char lineFeed = 0;
+    int numberOfDatabases = 0;
+    int dbNameLength = 0;
+    int recordRead = 0;
+    int recordDBVersion = 0;
+    int databaseHandle = 0;
+    int fieldLength = 0;
+    int fieldType = 0;
+    int dbID = 0;
+    int recordLength = 0;
+    int uid = 0;
 
-      Record record = null;
-      InteractivePagerBackup database = null;
-      FileInputStream input = null;
+    Record record = null;
+    InteractivePagerBackup database = null;
+    FileInputStream input = null;
 
-      try
-      {
-         input = new FileInputStream( fileName );
-         FileChannel fc = input.getChannel();
+    try {
+      input = new FileInputStream( fileName );
+      FileChannel fc = input.getChannel();
 
-         // Start reading in the header state
-         ReadingState state = ReadingState.HEADER;
-         while ( fc.position() < fc.size() )
-         {
-            switch ( state )
-            {
-               case HEADER:
-                  for ( int i = 0; i < "Inter@ctive Pager Backup/Restore File".length(); i++ )
-                  {
-                     input.read();
-                  }
-                  state = ReadingState.LINEFEED;
-                  break;
-
-               case LINEFEED:
-                  lineFeed = (char) input.read();
-                  state = ReadingState.VERSION;
-                  break;
-
-               case VERSION:
-                  database = new InteractivePagerBackup( input.read(), lineFeed );
-                  state = ReadingState.DATABASECOUNT;
-                  break;
-
-               case DATABASECOUNT:
-                  numberOfDatabases = input.read() << 8;
-                  numberOfDatabases |= input.read();
-                  state = ReadingState.DATABASENAMESEPARATOR;
-                  break;
-
-               // Just eat it because we know the terminating null will be in
-               // the name anyway
-               case DATABASENAMESEPARATOR:
-                  input.read();
-                  state = ReadingState.DATABASENAMELENGTH;
-                  break;
-
-               case DATABASENAMELENGTH:
-                  dbNameLength = input.read();
-                  dbNameLength |= input.read() << 8;
-                  state = ReadingState.DATABASENAME;
-                  break;
-
-               case DATABASENAME:
-                  StringBuffer buffer = new StringBuffer();
-                  // Read everything but the terminating null
-                  for ( int i = 0; i < dbNameLength - 1; i++ )
-                  {
-                     buffer.append( (char) input.read() );
-                  }
-
-                  database.addDatabase( buffer.toString() );
-
-                  // Eat null/separator
-                  input.read();
-
-                  if ( database.databaseNames().size() < numberOfDatabases )
-                  {
-                     state = ReadingState.DATABASENAMELENGTH;
-                  }
-                  else
-                  {
-                     state = ReadingState.DATABASEID;
-                  }
-                  break;
-
-               case DATABASEID:
-                  dbID = input.read();
-                  dbID |= input.read() << 8;
-                  state = ReadingState.RECORDLENGTH;
-                  break;
-
-               case RECORDLENGTH:
-                  recordLength = input.read();
-                  recordLength |= input.read() << 8;
-                  recordLength |= input.read() << 16;
-                  recordLength |= input.read() << 24;
-                  recordRead = 0;
-                  state = ReadingState.RECORDDBEVERSION;
-                  break;
-
-               case RECORDDBEVERSION:
-                  recordRead++;
-                  recordDBVersion = input.read();
-                  state = ReadingState.DATABASERECORDHANDLE;
-                  break;
-
-               case DATABASERECORDHANDLE:
-                  databaseHandle = input.read();
-                  databaseHandle |= input.read() << 8;
-                  recordRead += 2;
-                  state = ReadingState.RECORDUNIQUEID;
-                  break;
-
-               case RECORDUNIQUEID:
-                  uid = input.read();
-                  uid |= input.read() << 8;
-                  uid |= input.read() << 16;
-                  uid |= input.read() << 24;
-                  recordRead += 4;
-                  record = database.createRecord( dbID, recordDBVersion, uid, recordLength );
-                  record.setRecordDBHandle( databaseHandle );
-                  state = ReadingState.FIELDLENGTH;
-                  break;
-
-               case FIELDLENGTH:
-                  fieldLength = input.read();
-                  fieldLength |= input.read() << 8;
-                  recordRead += 2;
-                  state = ReadingState.FIELDTYPE;
-                  break;
-
-               case FIELDTYPE:
-                  fieldType = input.read();
-                  recordRead++;
-                  state = ReadingState.FIELDDATA;
-                  break;
-
-               case FIELDDATA:
-                  char[] dataBuffer = new char[ fieldLength ];
-                  for ( int i = 0; i < fieldLength; i++ )
-                  {
-                     dataBuffer[ i ] = (char) input.read();
-                  }
-
-                  record.addField( fieldType, dataBuffer );
-                  recordRead += fieldLength;
-
-                  if ( recordRead < record.getLength() )
-                  {
-                     state = ReadingState.FIELDLENGTH;
-                  }
-                  else
-                  {
-                     state = ReadingState.DATABASEID;
-                  }
-                  break;
+      // Start reading in the header state
+      ReadingState state = ReadingState.HEADER;
+      while ( fc.position() < fc.size() ) {
+        switch ( state ) {
+          case HEADER:
+            for ( int i = 0; i < "Inter@ctive Pager Backup/Restore File".length(); i++ ) {
+              input.read();
             }
-         }
-      }
-      catch ( IOException exception )
-      {
-         throw exception;
-      }
-      finally
-      {
-         input.close();
-      }
+            state = ReadingState.LINEFEED;
+            break;
 
-      return database;
-   }
+          case LINEFEED:
+            lineFeed = (char) input.read();
+            state = ReadingState.VERSION;
+            break;
+
+          case VERSION:
+            database = new InteractivePagerBackup( input.read(), lineFeed );
+            state = ReadingState.DATABASECOUNT;
+            break;
+
+          case DATABASECOUNT:
+            numberOfDatabases = input.read() << 8;
+            numberOfDatabases |= input.read();
+            state = ReadingState.DATABASENAMESEPARATOR;
+            break;
+
+          // Just eat it because we know the terminating null will be in
+          // the name anyway
+          case DATABASENAMESEPARATOR:
+            input.read();
+            state = ReadingState.DATABASENAMELENGTH;
+            break;
+
+          case DATABASENAMELENGTH:
+            dbNameLength = input.read();
+            dbNameLength |= input.read() << 8;
+            state = ReadingState.DATABASENAME;
+            break;
+
+          case DATABASENAME:
+            StringBuffer buffer = new StringBuffer();
+            // Read everything but the terminating null
+            for ( int i = 0; i < dbNameLength - 1; i++ ) {
+              buffer.append( (char) input.read() );
+            }
+
+            database.addDatabase( buffer.toString() );
+
+            // Eat null/separator
+            input.read();
+
+            if ( database.databaseNames().size() < numberOfDatabases ) {
+              state = ReadingState.DATABASENAMELENGTH;
+            } else {
+              state = ReadingState.DATABASEID;
+            }
+            break;
+
+          case DATABASEID:
+            dbID = input.read();
+            dbID |= input.read() << 8;
+            state = ReadingState.RECORDLENGTH;
+            break;
+
+          case RECORDLENGTH:
+            recordLength = input.read();
+            recordLength |= input.read() << 8;
+            recordLength |= input.read() << 16;
+            recordLength |= input.read() << 24;
+            recordRead = 0;
+            state = ReadingState.RECORDDBEVERSION;
+            break;
+
+          case RECORDDBEVERSION:
+            recordRead++;
+            recordDBVersion = input.read();
+            state = ReadingState.DATABASERECORDHANDLE;
+            break;
+
+          case DATABASERECORDHANDLE:
+            databaseHandle = input.read();
+            databaseHandle |= input.read() << 8;
+            recordRead += 2;
+            state = ReadingState.RECORDUNIQUEID;
+            break;
+
+          case RECORDUNIQUEID:
+            uid = input.read();
+            uid |= input.read() << 8;
+            uid |= input.read() << 16;
+            uid |= input.read() << 24;
+            recordRead += 4;
+            record = database.createRecord( dbID, recordDBVersion, uid, recordLength );
+            record.setRecordDBHandle( databaseHandle );
+            state = ReadingState.FIELDLENGTH;
+            break;
+
+          case FIELDLENGTH:
+            fieldLength = input.read();
+            fieldLength |= input.read() << 8;
+            recordRead += 2;
+            state = ReadingState.FIELDTYPE;
+            break;
+
+          case FIELDTYPE:
+            fieldType = input.read();
+            recordRead++;
+            state = ReadingState.FIELDDATA;
+            break;
+
+          case FIELDDATA:
+            char[] dataBuffer = new char[ fieldLength ];
+            for ( int i = 0; i < fieldLength; i++ ) {
+              dataBuffer[ i ] = (char) input.read();
+            }
+
+            record.addField( fieldType, dataBuffer );
+            recordRead += fieldLength;
+
+            if ( recordRead < record.getLength() ) {
+              state = ReadingState.FIELDLENGTH;
+            } else {
+              state = ReadingState.DATABASEID;
+            }
+            break;
+        }
+      }
+    } catch ( IOException exception ) {
+      throw exception;
+    } finally {
+      input.close();
+    }
+
+    return database;
+  }
 }
--- src/org/quaternions/ipddump/data/SMSMessage.java
+++ src/org/quaternions/ipddump/data/SMSMessage.java
@@ -14,267 +14,232 @@ import java.util.Map;
  * @author borkholder
  * @date Jan 1, 2008
  */
-public class SMSMessage extends Record implements Comparable<SMSMessage>
-{
-   /**
-    * The map from the name of the field to the field value.
-    */
-   protected final Map<String, String> fields;
+public class SMSMessage extends Record implements Comparable<SMSMessage> {
+  /**
+   * The map from the name of the field to the field value.
+   */
+  protected final Map<String, String> fields;
 
-   /**
-    * The list of field names this record keeps.
-    */
-   protected static final List<String> fieldNames;
+  /**
+   * The list of field names this record keeps.
+   */
+  protected static final List<String> fieldNames;
 
-   /**
-    * The date the SMS was sent;
-    */
-   protected Date                      sent;
+  /**
+   * The date the SMS was sent;
+   */
+  protected Date                      sent;
 
-   /**
-    * The date the SMS was received.
-    */
-   protected Date                      received;
+  /**
+   * The date the SMS was received.
+   */
+  protected Date                      received;
 
-   /**
-    * Was the SMS sent from the phone, or received?
-    */
-   protected Boolean                   wasSent;
+  /**
+   * Was the SMS sent from the phone, or received?
+   */
+  protected Boolean                   wasSent;
 
-   /**
-    * The far number.
-    */
-   protected String                    number;
+  /**
+   * The far number.
+   */
+  protected String                    number;
 
-   /**
-    * The text of the SMS.
-    */
-   protected String                    text;
+  /**
+   * The text of the SMS.
+   */
+  protected String                    text;
 
-   /**
-    * Initialize the list of field names with all the fields contained in this
-    * record.
-    */
-   static
-   {
-      fieldNames = new LinkedList<String>();
-      fieldNames.add( "sent" );
-      fieldNames.add( "received" );
-      fieldNames.add( "sent?" );
-      fieldNames.add( "number" );
-      fieldNames.add( "text" );
-   }
+  /**
+   * Initialize the list of field names with all the fields contained in this
+   * record.
+   */
+  static {
+    fieldNames = new LinkedList<String>();
+    fieldNames.add( "sent" );
+    fieldNames.add( "received" );
+    fieldNames.add( "sent?" );
+    fieldNames.add( "number" );
+    fieldNames.add( "text" );
+  }
 
-   /**
-    * Creates a new record with all provided data.
-    *
-    * @param dbID
-    *           The database id
-    * @param dbVersion
-    *           The database version
-    * @param uid
-    *           The unique identifier of this record
-    * @param recordLength
-    *           The length of the record
-    */
-   SMSMessage( int dbID, int dbVersion, int uid, int recordLength )
-   {
-      super( dbID, dbVersion, uid, recordLength );
-      fields = new HashMap<String, String>();
-   }
+  /**
+   * Creates a new record with all provided data.
+   *
+   * @param dbID The database id
+   * @param dbVersion The database version
+   * @param uid The unique identifier of this record
+   * @param recordLength The length of the record
+   */
+  SMSMessage( int dbID, int dbVersion, int uid, int recordLength ) {
+    super( dbID, dbVersion, uid, recordLength );
+    fields = new HashMap<String, String>();
+  }
 
-   /**
-    * Gets the date the SMS was sent.
-    *
-    * @return The sent date
-    */
-   public Date getSent()
-   {
-      return sent;
-   }
+  /**
+   * Gets the date the SMS was sent.
+   *
+   * @return The sent date
+   */
+  public Date getSent() {
+    return sent;
+  }
 
-   /**
-    * Gets the date the SMS was received.
-    *
-    * @return The received date
-    */
-   public Date getReceived()
-   {
-      return received;
-   }
+  /**
+   * Gets the date the SMS was received.
+   *
+   * @return The received date
+   */
+  public Date getReceived() {
+    return received;
+  }
 
-   /**
-    * Was the SMS sent from the phone or received.
-    *
-    * @return True if sent
-    */
-   public boolean wasSent()
-   {
-      return wasSent;
-   }
+  /**
+   * Was the SMS sent from the phone or received.
+   *
+   * @return True if sent
+   */
+  public boolean wasSent() {
+    return wasSent;
+  }
 
-   /**
-    * Gets the far phone number.
-    *
-    * @return The phone number
-    */
-   public String getNumber()
-   {
-      return number;
-   }
+  /**
+   * Gets the far phone number.
+   *
+   * @return The phone number
+   */
+  public String getNumber() {
+    return number;
+  }
 
-   /**
-    * Gets the text of the SMS.
-    *
-    * @return The SMS text
-    */
-   public String getText()
-   {
-      return text;
-   }
+  /**
+   * Gets the text of the SMS.
+   *
+   * @return The SMS text
+   */
+  public String getText() {
+    return text;
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public void addField( int type, char[] data )
-   {
-      StringBuilder builder = null;
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void addField( int type, char[] data ) {
+    StringBuilder builder = null;
 
-      switch ( type )
-      {
-         case 4:
-            text = new StringBuilder().append( data ).toString();
-            fields.put( "text", text );
-            break;
+    switch ( type ) {
+      case 4:
+        text = new StringBuilder().append( data ).toString();
+        fields.put( "text", text );
+        break;
 
-         case 2:
-            builder = new StringBuilder();
-            for ( char c : data )
-            {
-               if ( Character.isLetterOrDigit( c ) ||
-                    c == '#' ||
-                    c == '*' ||
-                    c == '(' ||
-                    c == ')' ||
-                    c == '.' ||
-                    c == '-' ||
-                    c == '+' )
-               {
-                  builder.append( c );
-               }
-            }
+      case 2:
+        builder = new StringBuilder();
+        for ( char c : data ) {
+          if ( Character.isLetterOrDigit( c ) || c == '#' || c == '*' || c == '(' || c == ')' || c == '.' || c == '-' || c == '+' ) {
+            builder.append( c );
+          }
+        }
 
-            number = builder.toString();
-            fields.put( "number", number );
-            break;
+        number = builder.toString();
+        fields.put( "number", number );
+        break;
 
-         case 9:
-            // This is a sequence number and we don't care about it for now.
-            // The sequence number seems to apply only if it was sent from the
-            // phone in the IPD.
-            break;
+      case 9:
+        // This is a sequence number and we don't care about it for now.
+        // The sequence number seems to apply only if it was sent from the
+        // phone in the IPD.
+        break;
 
-         case 7:
-            // This is some fixed number for my phone, I probably don't care
-            // about it
-            break;
+      case 7:
+        // This is some fixed number for my phone, I probably don't care
+        // about it
+        break;
 
-         case 11:
-            // This is also inconsequential, for me it's 0000 for the first sms,
-            // 1000 for all the rest
-            break;
+      case 11:
+        // This is also inconsequential, for me it's 0000 for the first sms,
+        // 1000 for all the rest
+        break;
 
-         case 1:
-            long val0 = 0;
-            for ( int i = 13; i < 21; i++ )
-            {
-               val0 |= (long) data[ i ] << ( ( i - 13 ) * 8 );
-            }
+      case 1:
+        long val0 = 0;
+        for ( int i = 13; i < 21; i++ ) {
+          val0 |= (long) data[ i ] << ( ( i - 13 ) * 8 );
+        }
 
-            long val1 = 0;
-            for ( int i = 21; i < 29; i++ )
-            {
-               val1 |= (long) data[ i ] << ( ( i - 21 ) * 8 );
-            }
+        long val1 = 0;
+        for ( int i = 21; i < 29; i++ ) {
+          val1 |= (long) data[ i ] << ( ( i - 21 ) * 8 );
+        }
 
-            if ( data[ 0 ] == 0 )
-            {
-               sent = new Date( val0 );
-               received = new Date( val1 );
-               wasSent = true;
-            }
-            else
-            {
-               sent = new Date( val1 );
-               received = new Date( val0 );
-               wasSent = false;
-            }
+        if ( data[ 0 ] == 0 ) {
+          sent = new Date( val0 );
+          received = new Date( val1 );
+          wasSent = true;
+        } else {
+          sent = new Date( val1 );
+          received = new Date( val0 );
+          wasSent = false;
+        }
 
-            fields.put( "sent", sent.toString() );
-            fields.put( "received", received.toString() );
-            fields.put( "sent?", wasSent.toString() );
+        fields.put( "sent", sent.toString() );
+        fields.put( "received", received.toString() );
+        fields.put( "sent?", wasSent.toString() );
 
-            break;
+        break;
 
-         default:
-            // Should be no default
-      }
-   }
+      default:
+        // Should be no default
+    }
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public Map<String, String> fields()
-   {
-      return Collections.<String, String> unmodifiableMap( fields );
-   }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Map<String, String> fields() {
+    return Collections.<String, String> unmodifiableMap( fields );
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public List<String> fieldNames()
-   {
-      return getFieldNames();
-   }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public List<String> fieldNames() {
+    return getFieldNames();
+  }
 
-   /**
-    * Gets the list of field names that may be assigned by this record.
-    *
-    * @return An unmodifiable list of field names
-    */
-   public static List<String> getFieldNames()
-   {
-      return Collections.<String> unmodifiableList( fieldNames );
-   }
+  /**
+   * Gets the list of field names that may be assigned by this record.
+   *
+   * @return An unmodifiable list of field names
+   */
+  public static List<String> getFieldNames() {
+    return Collections.<String> unmodifiableList( fieldNames );
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public int compareTo( SMSMessage o )
-   {
-      if ( sent.compareTo( o.sent ) != 0 )
-      {
-         return sent.compareTo( o.sent );
-      }
-      else
-      {
-         return received.compareTo( o.received );
-      }
-   }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int compareTo( SMSMessage o ) {
+    if ( sent.compareTo( o.sent ) != 0 ) {
+      return sent.compareTo( o.sent );
+    } else {
+      return received.compareTo( o.received );
+    }
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public String toString() {
-     if ( wasSent() ) {
-       return "To: " + getNumber() + " - " + getText();
-     } else {
-       return "From: " + getNumber() + " - " + getText();
-     }
-   }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String toString() {
+    if ( wasSent() ) {
+      return "To: " + getNumber() + " - " + getText();
+    } else {
+      return "From: " + getNumber() + " - " + getText();
+    }
+  }
 }
