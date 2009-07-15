376b2650b1441c29e2c50e117baac887b9ec9204
borkhobs <borkhobs@f63e4cbc-6a43-0410-8324-03926db9f09f>
2009-07-15 02:29:10 +0000

    Figured out the units in the time fields, but I'm still not sure what the zero point is and how they use timezones.

--- src/org/quaternions/ipddump/data/Task.java
+++ src/org/quaternions/ipddump/data/Task.java
@@ -1,18 +1,19 @@
 package org.quaternions.ipddump.data;
 
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * A contact is a record representing contact information stored in the address
  * book.
- *
+ * 
  * @author Jimmys Daskalakis - jimdaskalakis01@yahoo.gr
  * @created Jun 20, 2009
  */
 public class Task extends Record implements Comparable<Task> {
   /**
    * Creates a new record with all provided data.
-   *
+   * 
    * @param dbID
    *          The database id
    * @param dbVersion
@@ -38,18 +39,17 @@ public class Task extends Record implements Comparable<Task> {
       break;
 
     case 3:
-//      fields.put("Notes", makeString(data));
+      fields.put("Notes", makeString(data));
       break;
 
-//    case 5:
-//    case 6:
-      // seem to be the same all the time
-      // time due
-//      break;
+    case 5:
+    case 6:
+      fields.put("Due", makeDate(data).toString());
+      break;
 
     case 8:
       boolean due = data[0] > 0;
-//      fields.put("Due", due ? "true" : "false");
+      fields.put("Due", due ? "true" : "false");
       break;
 
     case 10:
@@ -62,28 +62,28 @@ public class Task extends Record implements Comparable<Task> {
       status |= data[1] << 8;
       status |= data[2] << 16;
       status |= data[3] << 24;
-//      switch (status) {
-//      case 0:
-//        fields.put("Status", "Not Started");
-//        break;
-//
-//      case 1:
-//        fields.put("Status", "In Progress");
-//        break;
-//
-//      case 2:
-//        fields.put("Status", "Completed");
-//        break;
-//
-//      case 3:
-//        fields.put("Status", "Waiting");
-//        break;
-//
-//      case 4:
-//        fields.put("Status", "Deferred");
-//        break;
-//      }
+      switch (status) {
+      case 0:
+        fields.put("Status", "Not Started");
         break;
+
+      case 1:
+        fields.put("Status", "In Progress");
+        break;
+
+      case 2:
+        fields.put("Status", "Completed");
+        break;
+
+      case 3:
+        fields.put("Status", "Waiting");
+        break;
+
+      case 4:
+        fields.put("Status", "Deferred");
+        break;
+      }
+      break;
 
     // Priority
     case 14:
@@ -91,23 +91,23 @@ public class Task extends Record implements Comparable<Task> {
       priority |= data[1] << 8;
       priority |= data[2] << 16;
       priority |= data[3] << 24;
-//      switch (priority) {
-//      case 0:
-//        fields.put("Priority", "Low");
-//        break;
-//
-//      case 1:
-//        fields.put("Priority", "Normal");
-//        break;
-//
-//      case 2:
-//        fields.put("Priority", "High");
-//        break;
-//      }
+      switch (priority) {
+      case 0:
+        fields.put("Priority", "Low");
         break;
 
+      case 1:
+        fields.put("Priority", "Normal");
+        break;
+
+      case 2:
+        fields.put("Priority", "High");
+        break;
+      }
+      break;
+
     case 15:
-      // Reminder time
+      fields.put("Reminder", makeDate(data).toString());
       break;
 
     // Timezone
@@ -118,7 +118,7 @@ public class Task extends Record implements Comparable<Task> {
     default:
       StringBuilder builder = new StringBuilder();
       for (char c : data) {
-        builder.append(String.format("%h", c));
+        builder.append(String.format("%2h", c));
       }
       fields.put("" + type, builder.toString());
       break;
@@ -128,11 +128,11 @@ public class Task extends Record implements Comparable<Task> {
       if (existing == null) {
         existing = "";
       } else {
-        //Microsoft Outlook Style. If sepated by commas then the CSV brakes.
+        // Microsoft Outlook Style. If sepated by commas then the CSV brakes.
         existing += ";";
       }
 
-//      fields.put("Categories", existing + makeString(data));
+      // fields.put("Categories", existing + makeString(data));
       break;
     }
   }
@@ -150,5 +150,26 @@ public class Task extends Record implements Comparable<Task> {
   protected String makeString(char[] data) {
     String str = new String(data);
     return str.substring(0, str.length() - 1);
+  }
+
+  /**
+   * Making a date is not simple, RIM doesn't use the standard
+   * "seconds since the epoch". The unit is minutes, but the zero point is
+   * somewhere around the start of 1900.
+   */
+  protected Date makeDate(char[] data) {
+    long time = data[0] << 0;
+    time |= data[1] << 8;
+    time |= data[2] << 16;
+    time |= data[3] << 24;
+
+    // Turn into milliseconds units
+    time *= 60 * 1000;
+    // Zero out at Jan 1, 1900
+    time -= 2208970740000L;
+    // Make the offset be the local timezone, minus the odd 61 minutes
+    int offset = TimeZone.getDefault().getOffset(time);
+    offset -= 61 * 60 * 1000;
+    return new Date(time + offset);
   }
 }
