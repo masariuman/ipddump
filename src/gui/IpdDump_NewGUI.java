13d9f92f545515fc868402a1163561cd075cc0a0
jimdaskalakis01 <jimdaskalakis01@gmail.com>
2009-08-10 12:18:08 +0000

    Fix: Warping lines for the big tooltips

--- src/gui/IpdDump_NewGUI.java
+++ src/gui/IpdDump_NewGUI.java
@@ -21,6 +21,7 @@ import javax.swing.table.TableModel;
 import ipddump.*;
 import ipddump.data.*;
 import ipddump.tools.*;
+import java.text.BreakIterator;
 
 /**
  *
@@ -1536,7 +1537,8 @@ public class IpdDump_NewGUI extends javax.swing.JFrame {
                 tollTipText = CallLogs.toPlainText(new int[]{hoveredRow});
             }
             tollTipText = tollTipText.substring(0, tollTipText.length() - 1);
-            jTableTemp.setToolTipText("<html>" + tollTipText.replace("\n", "<br>") + "</html>");
+            tollTipText=wrap("<html>" + tollTipText.replace("\n", "<br>") + "</html>", 50);
+            jTableTemp.setToolTipText(tollTipText);
         } else {
             jTableTemp.setToolTipText(null);
         }
@@ -1832,6 +1834,97 @@ public class IpdDump_NewGUI extends javax.swing.JFrame {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(stringSelection, (ClipboardOwner) null);
     }
+
+    /**
+     * <p>Wrap text at a certain line length. Defaults to HTML line break.</p>
+     *
+     * <pre>
+     * StringUtil.wrap("1234512345", 5) = 12345<br>12345
+     * StringUtil.wrap("12345", 10) = 12345
+     * </pre>
+     *
+     * @param string The String
+     * @param wrapLength The position to create a line break
+     *
+     * @return String
+     */
+    private static String wrap(String string, int wrapLength) {
+        return wrap(string, null, wrapLength);
+    }
+
+    /**
+   *
+   * <pre>
+   * StringUtil.wrap("Hello World!, "<br>", 9) = Hello<br>World!
+   * StringUtil.wrap("1234512345", "<br>", 5) = 1234512345
+   * StringUtil.wrap("12345", 10) = 12345
+   *</pre>
+   *
+   * @param string The String
+   * @param lineSeparator Line break
+   * @param wrapLength The position to create a line break
+   *
+   * @return String
+   */
+  public static String wrap(String string, String lineSeparator, int wrapLength) {
+    //string = "Das ist ein ganz stink-normaler demo-text, mit dem ich das Verhalten dieses Wrapper testen moechte, ohne dass ein Langes Wort wie Schifffahrtsmeistereiangestelltenbeauftragter irgendwie Aerger machen koennte";
+
+    // Null or blank string should return an empty ("") string
+    if (isBlank(string)) {
+      return "";
+    }
+
+    int stringLength = string.length();
+
+    if (stringLength > wrapLength) {
+      //Ensure.ensureArrayIndex(wrapLength, 1, Integer.MAX_VALUE);
+
+      // Default to HTML line break since web app is  client
+      if (isBlank(lineSeparator)) {
+        lineSeparator = "<br>";
+      }
+
+      StringBuffer sb = new StringBuffer(stringLength
+          + ((stringLength / wrapLength) * 2 * lineSeparator.length()));
+      BreakIterator lineIterator = BreakIterator.getLineInstance();
+      lineIterator.setText(string);
+      int start = lineIterator.first();
+      int lineStart = start;
+
+      for (int end = lineIterator.next(); end != BreakIterator.DONE; start = end, end = lineIterator.next()) {
+        if (end - lineStart < wrapLength) {
+          sb.append(string.substring(start, end));
+        }
+        else {
+          // wrap
+          if (true || end - start < wrapLength) {
+            sb.append(lineSeparator);
+            sb.append(string.substring(start, end));
+          }
+          else {
+            // TODO
+            // truncate
+          }
+          lineStart = end;
+        }
+      }
+      string = sb.toString();
+    }
+
+    return string;
+  }
+
+    /**
+     * Check to see if the string has no value
+     *
+     * @param String the string
+     *
+     * @return boolean (true meaning no value)
+     */
+    private static boolean isBlank(String string) {
+        return (string == null || string.trim().equals(""));
+    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JFileChooser IpdChooser;
     private javax.swing.JFrame MessageFrame;
